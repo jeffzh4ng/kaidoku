10080d1234c0bf13d7eaab84893476579b0b9366
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-05-25 09:18:51 -0400

    Create rough Fixed XOR functionality (solution 2) in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use fuin::encode::{ByteToHexEncoder, HexToByteDecoder};
+
 fn main() {
     println!(
         " 
@@ -11,4 +13,15 @@ fn main() {
     888     \"Y88888 888 888  888
     "
     );
+
+    let a = HexToByteDecoder::new("1c0111001f010100061a024b53535009181c");
+    let b = HexToByteDecoder::new("686974207468652062756c6c277320657965");
+    let c = a
+        .unwrap()
+        .zip(b.unwrap())
+        .map(|(x, y)| x ^ y)
+        .collect::<Vec<u8>>();
+
+    let c_pretty_print = ByteToHexEncoder::new(c.into_iter()).collect::<String>();
+    println!("{}", c_pretty_print);
 }

f81305358bdeb628c9f7c22c71234bd7cc4df89a
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-05-30 15:40:18 -0400

    Create attack on multiline single byte XOR cipher

--- src/attack.rs
+++ src/attack.rs
@@ -2,18 +2,18 @@ use std::{collections::HashMap, io};
 
 use crate::crypto;
 
-pub fn single_byte_xor_attack(cipher_text: Vec<u8>) -> String {
-    const FOO: &str = "ETAOIN SHRDLU";
-    fn score(s: &str) -> i32 {
-        s.chars().fold(0, |acc, c| {
-            if FOO.contains(c) {
-                return acc + 1;
-            }
+const ENGLISH_FREQ: &str = "QZXJKVBWPYGMCFULDRHS NIOTAE";
+pub fn score(s: &str) -> i32 {
+    s.chars().fold(0, |acc, c| {
+        if ENGLISH_FREQ.contains(c) {
+            return acc + ENGLISH_FREQ.chars().position(|x| x == c).unwrap() as i32 + 1;
+        }
 
-            acc
-        })
-    }
+        acc
+    })
+}
 
+pub fn single_byte_xor_attack(cipher_text: Vec<u8>) -> Option<String> {
     // ciphertext: 1111 0000
     // keyspace:   0000 0001
     // plaintext:  1111 0001
@@ -37,13 +37,15 @@ pub fn single_byte_xor_attack(cipher_text: Vec<u8>) -> String {
             .collect::<Result<Vec<u8>, io::Error>>()
             .unwrap();
 
+        let s = String::from_utf8(plain_bytes.clone());
+
         // check if plain bytes are invalid utf8
-        if String::from_utf8(plain_bytes.clone()).is_err() {
+        if s.is_err() {
             continue;
         }
 
         // convert bytes to string, calculate the score, and store it in the map
-        let plain_text = String::from_utf8(plain_bytes).unwrap();
+        let plain_text = s.unwrap();
         let score = score(&plain_text);
         plain_text_scores.insert(plain_text, score);
     }
@@ -54,8 +56,23 @@ pub fn single_byte_xor_attack(cipher_text: Vec<u8>) -> String {
         .collect::<Vec<(String, i32)>>();
     plain_text_scores_tuples.sort_by(|a, b| b.1.cmp(&a.1));
 
+    if plain_text_scores_tuples.is_empty() {
+        return None; // no valid plain text
+    }
+
+    // if there's a tie, warn
+    if plain_text_scores_tuples[0].1 == plain_text_scores_tuples[1].1 {
+        println!("warning: there's a tie");
+        println!("one: {:?}", &plain_text_scores_tuples[0].0);
+        println!("two: {:?}", &plain_text_scores_tuples[1].0);
+    }
+
+    if !&plain_text_scores_tuples[0].0.as_bytes().is_ascii() {
+        return None;
+    }
+
     // select the plain text with the highest score
     let plain_text = &plain_text_scores_tuples[0].0;
 
-    plain_text.to_string()
+    Some(plain_text.to_string())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{fs, io, io::BufRead, path};
 
 use fuin::{attack, encode};
 
@@ -16,6 +16,7 @@ fn main() {
     "
     );
 
+    // challenge 3: single byte XOR attack
     let cipher_text = encode::hex::HexToByteDecoder::new(
         "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".chars(),
     )
@@ -23,5 +24,42 @@ fn main() {
     .unwrap();
 
     let plain_text = attack::single_byte_xor_attack(cipher_text);
-    println!("plain_text_with_highest_score: {}", plain_text)
+    println!("plain_text_with_highest_score: {}", plain_text.unwrap());
+
+    // challenge 4: single byte XOR attack from file
+    let path = path::Path::new("/Users/jeff/Documents/repos/fuin/src/single-byte-xor.txt");
+    let display = path.display();
+    let file = match fs::File::open(path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut high_score = 0;
+    let mut plain_text_with_high_score = String::new();
+
+    let reader = io::BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                let cipher_text = encode::hex::HexToByteDecoder::new(line.chars())
+                    .collect::<Result<Vec<u8>, io::Error>>()
+                    .unwrap();
+
+                let plain_text = attack::single_byte_xor_attack(cipher_text);
+
+                if let Some(p) = plain_text {
+                    if high_score == 0 || attack::score(&p) > high_score {
+                        high_score = attack::score(&p);
+                        plain_text_with_high_score = p;
+                    }
+                }
+            }
+            Err(why) => println!("error reading line: {}", why),
+        }
+    }
+
+    println!(
+        "plain_text_with_highest_score: {}",
+        plain_text_with_high_score
+    );
 }

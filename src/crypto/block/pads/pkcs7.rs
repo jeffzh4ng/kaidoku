f1c524e065896a68ddc3b0f093ee7caec74b0c9a
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-07-26 07:44:10 -0400

    Create skeleton Padder impl for Pkcs7

--- src/crypto/block/pads/pkcs7.rs
+++ src/crypto/block/pads/pkcs7.rs
@@ -1,1 +1,17 @@
+use super::super::ciphers::BlockCipher;
+use super::Padder;
 
+struct Pkcs7 {}
+
+impl<C> Padder<C> for Pkcs7
+where
+    C: BlockCipher,
+{
+    fn pad(&self, input: super::UnpaddedBytes) -> Vec<C::Block> {
+        todo!()
+    }
+
+    fn unpad(&self, input: Vec<C::Block>) -> super::UnpaddedBytes {
+        todo!()
+    }
+}

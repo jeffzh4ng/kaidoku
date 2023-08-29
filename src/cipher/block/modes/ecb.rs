ace6bb1a1fdc1aae4025db88bb81644bd7e151a1
jeffzh4ng <zhang.jeffreyd@gmail.com>
2023-08-29 08:40:52 -0400

    Add tests for Ecb

--- src/cipher/block/modes/ecb.rs
+++ src/cipher/block/modes/ecb.rs
@@ -17,15 +17,27 @@ where
     _marker: marker::PhantomData<N>, // required since N ties C and P together without being used directly
 }
 
-impl<N: ArrayLength<u8>, C: BlockCipher<N>, P: Padder<N>> BlockMode<N, C, P> for Ecb<N, C, P> {
-    fn new(cipher: C, padder: P) -> Self {
+impl<N, C, P> Ecb<N, C, P>
+where
+    N: ArrayLength<u8>,
+    C: BlockCipher<N>,
+    P: Padder<N>,
+{
+    pub fn new(cipher: C, padder: P) -> Self {
         Ecb {
             cipher,
             padder,
             _marker: marker::PhantomData,
         }
     }
+}
 
+impl<N, C, P> BlockMode<N, C, P> for Ecb<N, C, P>
+where
+    N: ArrayLength<u8>,
+    C: BlockCipher<N>,
+    P: Padder<N>,
+{
     fn encrypt(&mut self, plaintext: Vec<u8>) -> Vec<u8> {
         let ciphertext_blocks = self
             .padder
@@ -40,7 +52,7 @@ impl<N: ArrayLength<u8>, C: BlockCipher<N>, P: Padder<N>> BlockMode<N, C, P> for
 
     fn decrypt(&mut self, ciphertext: Vec<u8>) -> Vec<u8> {
         let plaintext_blocks = ciphertext
-            .chunks_exact(N::to_usize())
+            .chunks_exact(N::to_usize()) // TODO: what do if input has remainder
             .map(|chunk| GenericArray::clone_from_slice(chunk))
             .map(|ciphertext_block| self.cipher.decrypt_block(ciphertext_block))
             .collect();
@@ -55,7 +67,27 @@ mod tests {
     use crate::cipher::block::{ciphers, pads};
 
     #[test]
-    fn test_encrypt_no_pad() {
+    fn test_encrypt_half_block() {
+        let key = b"YELLOW SUBMARINE";
+        let cipher = ciphers::aes::Aes::new(key.to_vec());
+        let padder = pads::pkcs7::Pkcs7::new();
+        let mut ecb = Ecb::new(cipher, padder);
+
+        let plaintext = b"ABCDEFGH";
+        let encrypted = ecb.encrypt(plaintext.to_vec());
+
+        #[rustfmt::skip]
+        let expected_output = [
+            0x11, 0x38, 0xF7, 0x44,
+            0xA4, 0x1E, 0x80, 0xA5,
+            0xC1, 0x84, 0xAC, 0xF0,
+            0x2F, 0x6A, 0xD8, 0x22    
+        ];
+        assert_eq!(encrypted, expected_output);
+    }
+
+    #[test]
+    fn test_encrypt_one_block() {
         let key = b"YELLOW SUBMARINE";
         let cipher = ciphers::aes::Aes::new(key.to_vec());
         let padder = pads::pkcs7::Pkcs7::new();
@@ -65,6 +97,37 @@ mod tests {
         let encrypted = ecb.encrypt(plaintext.to_vec());
         #[rustfmt::skip]
         let expected_output = [
+            0xf5, 0x45, 0xc0, 0x06,
+            0x06, 0x91, 0x26, 0xd9,
+            0xc0, 0xf9, 0x3f, 0xa7,
+            0xdd, 0x89, 0xab, 0x98,
+            // encrypted padding (entire block)
+            0x60, 0xfa, 0x36, 0x70,
+            0x7e, 0x45, 0xf4, 0x99,
+            0xdb, 0xa0, 0xf2, 0x5b,
+            0x92, 0x23, 0x01, 0xa5,
+        ];
+
+        assert_eq!(encrypted, expected_output);
+    }
+
+    #[test]
+    fn test_encrypt_two_blocks() {
+        let key = b"YELLOW SUBMARINE";
+        let cipher = ciphers::aes::Aes::new(key.to_vec());
+        let padder = pads::pkcs7::Pkcs7::new();
+        let mut ecb = Ecb::new(cipher, padder);
+
+        let plaintext = b"ABCDEFGHIJKLMNOPABCDEFGHIJKLMNOP";
+        let encrypted = ecb.encrypt(plaintext.to_vec());
+        #[rustfmt::skip]
+        let expected_output = [
+            // block one
+            0xf5, 0x45, 0xc0, 0x06,
+            0x06, 0x91, 0x26, 0xd9,
+            0xc0, 0xf9, 0x3f, 0xa7,
+            0xdd, 0x89, 0xab, 0x98,
+            // block two
             0xf5, 0x45, 0xc0, 0x06,
             0x06, 0x91, 0x26, 0xd9,
             0xc0, 0xf9, 0x3f, 0xa7,
